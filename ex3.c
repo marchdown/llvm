fc1960f89fa8e13126f0843309c3e642a11ae061
marchdown <marchdown@gmail.com>
2015-01-07 05:18:31 +0300

    inb4 valgrind

---
+++ ex3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  int age = 10;
+  int height = 72;
+  int n_mondays = 52*25;
+  
+  printf("I have had %d mondays in my life\n", n_mondays);
+  printf("%d Lines, they be printed\n", height);
+
+  return 0;
+  
+}
+
